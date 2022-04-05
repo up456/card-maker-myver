ce1fc9ba39255e50318f23b0e3263faa8b13e814
Hihyu <4up456@naver.com>
2022-04-05 12:08:33 +0900

    card컨테츠 layout 추가

--- src/components/card_preview/card_preview.jsx
+++ src/components/card_preview/card_preview.jsx
@@ -1,11 +1,32 @@
 import styles from './card_preview.module.css';
-
 import React from 'react';
+import defaultImage from './default_logo.png';
 
-const CardPreview = (props) => {
+const CardPreview = ({ cards }) => {
   return (
     <article className={styles.cardPreview}>
       <h2 className={styles.contentTitle}>Card Preview</h2>
+      <ul className={styles.previewList}>
+        {cards.map((card, index) => {
+          return (
+            <li key={index} className={styles.previewItem}>
+              <img
+                className={styles.previeItemImage}
+                src={defaultImage}
+                alt="profileImage"
+              />
+              <div className={styles.previewItemDescription}>
+                <div className={styles.name}>{card.name}</div>
+                <div className={styles.company}>{card.company}</div>
+                <div className={styles.line}></div>
+                <div className={styles.etc}>{card.title}</div>
+                <div className={styles.etc}>{card.email}</div>
+                <div className={styles.message}>"{card.message}"</div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </article>
   );
 };
--- src/components/maker_page/maker_page.jsx
+++ src/components/maker_page/maker_page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react/cjs/react.development';
+import { useEffect, useState } from 'react/cjs/react.development';
 import CardMaker from '../card_maker/card_maker';
 import CardPreview from '../card_preview/card_preview';
 
@@ -13,6 +13,36 @@ const Maker_page = ({ authService }) => {
     authService.logout();
     navigate('/');
   };
+
+  const [cards, setCards] = useState([
+    {
+      name: '용석',
+      company: '카카오',
+      light: 'Dark',
+      title: '프론트엔드',
+      email: '4up456@naver.com',
+      message: '안녕하세요. 존잘남 박용석입니다.',
+      image: '/public/images/default_logo.png',
+    },
+    {
+      name: '트위치',
+      company: '라이엇',
+      light: 'Dark',
+      title: '원거리 암살자',
+      email: 'tiwitch@naver.com',
+      message: '깜짝 놀랬징?',
+      image: '/public/images/default_logo.png',
+    },
+    {
+      name: '아이유',
+      company: '로엔',
+      light: 'Colorful',
+      title: '아이돌 여가수',
+      email: 'iu@naver.com',
+      message: '존예 여가수~!',
+      image: '/public/images/default_logo.png',
+    },
+  ]);
 
   useEffect(() => {
     authService.onAuthChange((user) => {
@@ -27,8 +57,8 @@ const Maker_page = ({ authService }) => {
       <section className={styles.makerPage}>
         <Header onLogout={onLogout} />
         <section className={styles.makerContents}>
-          <CardMaker />
-          <CardPreview />
+          <CardMaker cards={cards} />
+          <CardPreview cards={cards} />
         </section>
         <Footer />
       </section>
